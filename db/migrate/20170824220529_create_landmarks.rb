8a9d7cd31becc5651c01fc80d98d01944acc3f92
psnider30 <pbsnider@gmail.com>
2017-08-24 15:31:38 -0700

    Passing rspec tests for models

---
+++ db/migrate/20170824220529_create_landmarks.rb
@@ -0,0 +1,8 @@
+class CreateLandmarks < ActiveRecord::Migration
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+      t.integer :figure_id
+    end
+  end
+end
---
+++ db/migrate/20170824222833_add_year_completed_to_landmark.rb
@@ -0,0 +1,5 @@
+class AddYearCompletedToLandmark < ActiveRecord::Migration
+  def change
+    add_column :landmarks, :year_completed, :integer
+  end
+end
